e366668bab72e671c0d2e224e96f3c2ded8e3f95
Lucas Lee <l2lee@ucsd.edu>
2024-10-19 12:25:22 -0700

    comments in js file

--- source/js/footnotes.js
+++ source/js/footnotes.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const footnotes = document.querySelectorAll('sup[id^="fnref:"] a[rel="footnote"]');
+    const footnotes = document.querySelectorAll('sup[id^="fnref:"] a[rel="footnote"]'); // select all the anchor tags that have a rel attribute with a value of footnote and an ID that starts with fnref:
 
     footnotes.forEach(footnote => {
-        const footnoteId = footnote.getAttribute('href').slice(1);
-        const footnoteContent = document.getElementById(footnoteId).querySelector('p').innerHTML;
+        const footnoteId = footnote.getAttribute('href').slice(1); // removes # from the beginning of the string (ID)
+        const footnoteContent = document.getElementById(footnoteId).querySelector('p').innerHTML; // gets the content of the footnote, which is stored in the paragraph tag's innerHTML
 
         const popup = document.createElement('span');
         popup.className = 'footnote-popup';
